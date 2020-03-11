6778853c3e52a8160f1b6fb0a766f85953ea0230
Fenugreek Catina <fcatina@mi-q3.21-school.ru>
2020-03-12 00:50:17 +0300

    free_m_num_and_struct.c is norm

--- free_m_num_and_struct.c
+++ free_m_num_and_struct.c
@@ -6,7 +6,7 @@
 /*   By: fcatina <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 04:42:54 by fcatina           #+#    #+#             */
-/*   Updated: 2020/03/11 04:44:02 by fcatina          ###   ########.fr       */
+/*   Updated: 2020/03/12 00:49:40 by fcatina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 void	free_m_struct(t_fdf **m_struct)
 {
-  int     i;
+	int		i;
 
-  i = 0;
-  while(m_struct[i])
-    {
-      free(m_struct[i]);
-      m_struct[i] = NULL;
-      i++;
-    }
-  free(m_struct);
-  m_struct = NULL;
+	i = 0;
+	while (m_struct[i])
+	{
+		free(m_struct[i]);
+		m_struct[i] = NULL;
+		i++;
+	}
+	free(m_struct);
+	m_struct = NULL;
 }
 
 void	free_m_num(int **m_num)
 {
-  int     i;
+	int		i;
 
-  i = 0;
-  while(m_num[i])
-    {
-      free(m_num[i]);
-      m_num[i] = NULL;
-      i++;
-    }
-  free(m_num);
-  m_num = NULL;
+	i = 0;
+	while (m_num[i])
+	{
+		free(m_num[i]);
+		m_num[i] = NULL;
+		i++;
+	}
+	free(m_num);
+	m_num = NULL;
 }

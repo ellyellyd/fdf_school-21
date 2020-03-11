611a9ea6c2d66ccb9ac41dbbd8f01fc78c2d1fa3
Fenugreek Catina <fcatina@at-g6.21-school.ru>
2020-03-11 04:40:09 +0300

    seme norm fixed

--- do_key.c
+++ do_key.c
@@ -6,66 +6,58 @@
 /*   By: fcatina <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 00:32:30 by fcatina           #+#    #+#             */
-/*   Updated: 2020/03/11 00:35:00 by fcatina          ###   ########.fr       */
+/*   Updated: 2020/03/11 04:36:27 by fcatina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	do_key(int key, t_fdf **matrix)
+void	do_key_2(int key, t_fdf **m)
 {
-	if (key == 11)
-	{
-		if (matrix[0][0].back == 0)
-			matrix[0][0].back = 1;
-		else
-			matrix[0][0].back = 0;
-	}
-	if (key == 2 && matrix[0][0].z_scale > (-1) * (matrix[0][0].win_y / matrix[0][0].scale))
-		matrix[0][0].z_scale -= 1;
-	if (key == 32 && matrix[0][0].z_scale < matrix[0][0].win_y / matrix[0][0].scale)
-		matrix[0][0].z_scale += 1;
-	if (key == 24)
-		matrix[0][0].scale += 1;
-	if (key == 27)
-		matrix[0][0].scale -= 1;
-	if (key == 124 && matrix[0][0].win_x >= matrix[0][0].shift_x + 3)
-		matrix[0][0].shift_x += 3;
-	if (key == 123 &&  matrix[0][0].shift_x - 3 > 0)
-		matrix[0][0].shift_x -= 3;
-	if (key == 125 && matrix[0][0].win_y >= matrix[0][0].shift_y + 3)
-		matrix[0][0].shift_y += 3;
-	if (key == 126 && matrix[0][0].shift_y - 3 > 0)
-		matrix[0][0].shift_y -= 3;
-	if (key == 46 || key == 49)
-	{
-		if (key == 49)
-		{
-			if (matrix[0][0].is_iso == 0)
-				matrix[0][0].is_iso = 1;
-			else
-				matrix[0][0].is_iso = 0;
-		}
-		if (key == 46)
-		{
-			if (matrix[0][0].menu == 0)
-			{
-				matrix[0][0].menu = 1;
-				matrix[0][0].shift_x = matrix[0][0].win_x / 2;
-			}
-			else
-			{
-				matrix[0][0].menu = 0;
-				if (matrix[0][0].is_iso == 0)
-					matrix[0][0].shift_x = matrix[0][0].win_x / 3;
-			}
-		}
-	}
-	if (key == 15)
-		set_default(matrix);
-	if (key == 17)
-		matrix[0][0].angle += 0.1;
-	if (key == 1)
-		matrix[0][0].angle -= 0.1;
+  if (key == 49)
+    m[0][0].is_iso = 0 ? m[0][0].is_iso == 1 : m[0][0].is_iso == 0;
+  if (key == 46)
+    {
+      if (m[0][0].menu == 0)
+        {
+          m[0][0].menu = 1;
+          m[0][0].shift_x = m[0][0].win_x / 2;
+        }
+      else
+        {
+          m[0][0].menu = 0;
+          if (m[0][0].is_iso == 0)
+            m[0][0].shift_x = m[0][0].win_x / 3;
+        }
+    }
 }
 
+void	do_key(int key, t_fdf **m)
+{
+	if (key == 11)
+    m[0][0].back = 0 ? m[0][0].back == 1 : m[0][0].back == 0;
+	if (key == 2 && m[0][0].z_scale > (-1) * (m[0][0].win_y / m[0][0].scale))
+		m[0][0].z_scale -= 1;
+	if (key == 32 && m[0][0].z_scale < m[0][0].win_y / m[0][0].scale)
+		m[0][0].z_scale += 1;
+	if (key == 24)
+		m[0][0].scale += 1;
+	if (key == 27)
+		m[0][0].scale -= 1;
+	if (key == 124 && m[0][0].win_x >= m[0][0].shift_x + 3)
+		m[0][0].shift_x += 3;
+	if (key == 123 &&  m[0][0].shift_x - 3 > 0)
+		m[0][0].shift_x -= 3;
+	if (key == 125 && m[0][0].win_y >= m[0][0].shift_y + 3)
+		m[0][0].shift_y += 3;
+	if (key == 126 && m[0][0].shift_y - 3 > 0)
+		m[0][0].shift_y -= 3;
+	if (key == 15)
+		set_default(m);
+	if (key == 17)
+		m[0][0].angle += 0.1;
+	if (key == 1)
+		m[0][0].angle -= 0.1;
+	if (key == 46 || key == 49)
+    do_key_2(key, m);
+}
